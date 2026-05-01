caefc83198821a3740938e11ebe0f64bed8ea9f1
Sonukumar-br <sonukumar9982s@gmail.com>
2026-05-01 19:02:35 +0530

    Update cookie settings for secure authentication

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -33,8 +33,8 @@ export const signup = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
-      secure: false,
+      sameSite: "None",
+      secure: true,
       path: "/",
     });
 
@@ -68,8 +68,8 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "lax",
-      secure: false,
+      sameSite: "None",
+      secure: true,
       path: "/",
     });
 
@@ -92,8 +92,8 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    sameSite: "lax",
-    secure: false,
+    sameSite: "None",
+    secure: true,
     path: "/",
   });
 
